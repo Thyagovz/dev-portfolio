b1d8c8bb84b415f91a8a37d516bd68f2535bdf38
Thyago Euclides <flooddk@gmail.com>
2026-07-04 19:20:24 -0400

    sync prismic slices

---
+++ src/lib/slices/Hero/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import type { SliceComponentProps } from '@prismicio/svelte';
+
+	type Props = SliceComponentProps<Content.HeroSlice>;
+
+	const { slice }: Props = $props();
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
+<br />
+<strong>You can edit this slice directly in your code editor.</strong>
+</section>
