abfba795bed00144f6e2511dd8f019ae51b6fe01
Thyago Euclides <flooddk@gmail.com>
2026-07-07 15:22:38 -0400

    refactor TechList component: enhance GSAP animations and improve item rendering logic

--- src/lib/slices/TechList/index.svelte
+++ src/lib/slices/TechList/index.svelte
@@ -10,40 +10,68 @@
   gsap.registerPlugin(ScrollTrigger);
 
   let { slice }: { slice: Content.TechListSlice } = $props();
-
   let component: HTMLElement | undefined = $state();
+
+  onMount(() => {
+		const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+		if (prefersReducedMotion) {
+			return;
+		}
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: component,
+				start: 'top bottom',
+				end: 'bottom top',
+				scrub: 4
+			}
+		});
+
+		tl.fromTo(
+			'.tech-row',
+			{
+				x: (index) => {
+					return index % 2 === 0 ? gsap.utils.random(600, 400) : gsap.utils.random(-600, -400);
+				}
+			},
+			{
+				x: (index) => {
+					return index % 2 === 0 ? gsap.utils.random(-600, -400) : gsap.utils.random(600, 400);
+				},
+				ease: 'power1.inOut'
+			}
+		);
+	});
 </script>
 
 <section
-  data-slice-type={slice.slice_type}
-  data-slice-variation={slice.variation}
-  class="overflow-hidden"
-  bind:this={component}
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+	class="overflow-hidden"
+	bind:this={component}
 >
-  <Bounded as="div">
-    <Heading size="xl" class="mb-8" tag="h2">
-      {slice.primary.heading}
-    </Heading>
-  </Bounded>
+	<Bounded as="div">
+		<Heading size="xl" class="mb-8" tag="h2">
+			{slice.primary.heading}
+		</Heading>
+	</Bounded>
 
-  {#each slice.primary.item as item}
-    <div
-      class="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
-      aria-label={item.tech_name || undefined}
-    >
-      {#each Array.from({ length: 15 }) as _, index}
-        <span
-          class="tech-item text-8xl font-extrabold uppercase tracking-tighter"
-          style="color: {index === 7 && item.tech_color
-            ? item.tech_color
-            : 'inherit'}"
-        >
-          {item.tech_name}
-        </span>
-        <span class="text-3xl">
-          <IconCircle />
-        </span>
-      {/each}
-    </div>
-  {/each}
+	{#each slice.primary.item as { tech_color, tech_name }}
+		<div
+			class="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+			aria-label={tech_name || undefined}
+		>
+			{#each Array(15) as _, index}
+				<span
+					class="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+					style="color: {index === 7 && tech_color ? tech_color : 'inherit'}"
+				>
+					{tech_name}
+				</span>
+				<span class="text-3xl">
+					<IconCircle />
+				</span>
+			{/each}
+		</div>
+	{/each}
 </section>
